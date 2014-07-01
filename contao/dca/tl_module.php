f5673ba40b2e98114490946ce024e7105776347d
kozi <martin@kozianka.de>
2014-07-01 17:10:23 +0200

    Refactored to contao-fullcalendar

--- contao/dca/tl_module.php
+++ contao/dca/tl_module.php
@@ -13,4 +13,40 @@
  * @filesource
  */
 
-$GLOBALS['TL_DCA']['tl_module']['palettes']['fullcalendar'] = '{title_legend},name,headline,type;{fullcalendar_legend},cal_startDay;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['fullcalendar'] = '{title_legend},name,headline,type;
+{fullcalendar_legend},cal_startDay,fullcal_isRTL;
+{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['fullcal_weekNumbers'] = array(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_module']['fullcal_weekNumbers'],
+    'exclude'                 => true,
+    'inputType'               => 'checkbox',
+    'sql'                     => "char(1) NOT NULL default ''",
+    'eval'                    => array(),
+);
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['fullcal_weekMode'] = array(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_module']['fullcal_weekNumbers'],
+    'exclude'                 => true,
+    'inputType'               => 'checkbox',
+    'sql'                     => "char(1) NOT NULL default ''",
+    'eval'                    => array(),
+    // 'fixed' options = array('fixed', 'liquid', 'variable');
+);
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['fullcal_aspectRatio'] = array( // 1.35
+    'label'                   => &$GLOBALS['TL_LANG']['tl_module']['fullcal_weekNumbers'],
+    'exclude'                 => true,
+    'inputType'               => 'checkbox',
+    'sql'                     => "char(1) NOT NULL default ''",
+    'eval'                    => array(),
+);
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['fullcal_isRTL'] = array(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_module']['fullcal_isRTL'],
+    'exclude'                 => true,
+    'inputType'               => 'checkbox',
+    'sql'                     => "char(1) NOT NULL default ''",
+    'eval'                    => array(),
+);
