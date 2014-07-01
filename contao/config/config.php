fae553919bf4f8d1d6c564bc5e9ce7a691884fa0
kozi <martin@kozianka.de>
2014-07-01 23:16:14 +0200

    Refactored to contao-fullcalendar

--- contao/config/config.php
+++ contao/config/config.php
@@ -13,14 +13,6 @@
  * @filesource
  */
 
-$GLOBALS['BE_MOD']['system']['dav'] = array(
-    'tables'     => array('tl_fullcalendar_dav', 'tl_fullcalendar_mapping'),
-    'icon'       => 'system/modules/fullcalendar/assets/drive-network.png',
-    'stylesheet' => 'system/modules/fullcalendar/assets/backend.css',
-    'test'       => array('ContaoFullcalendar\WebdavManager', 'testConnection'),
-    'refresh'    => array('ContaoFullcalendar\WebdavManager', 'updateFiles')
-);
-
 $GLOBALS['BE_MOD']['content']['calendar']['fullcal'] = array('ContaoFullcalendar\CalendarSync', 'syncCal');
 
 $GLOBALS['FE_MOD']['events']['fullcalendar']         = 'ContaoFullcalendar\ModuleFullCalendar';
