2dc7c25b4c0bd7d92c66e4794005c401305b7615
Martin Kozianka <martin@kozianka.de>
2015-08-18 21:31:02 +0200

    Added english translation

---
+++ contao/languages/en/tl_module.php
@@ -0,0 +1,20 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_module']['fullcal_legend']         = 'FullCalendar';
+$GLOBALS['TL_LANG']['tl_module']['fullcal_range']          = ['Period of time', 'Period of time'];
+$GLOBALS['TL_LANG']['tl_module']['fullcal_weekNumbers']    = ['Show calendar weeks', 'Show a column with the week numbers'];
+$GLOBALS['TL_LANG']['tl_module']['fullcal_weekMode']       = ['Week mode', 'Week mode'];
+$GLOBALS['TL_LANG']['tl_module']['fullcal_aspectRatio']    = ['Aspect ratio', 'Aspect ratio [VALUE] x height'];
+$GLOBALS['TL_LANG']['tl_module']['fullcal_isRTL']          = ['Direction RTL', 'Direction RTL?'];
+
+$fullcalendarDesc = 'Comma seperated (attached buttons) or space (separate buttons) seperated list of following values: today, title, prev, next, month, agendaWeek, agendaDay';
+$GLOBALS['TL_LANG']['tl_module']['fullcal_header_left']    = ['Header (left)', $fullcalendarDesc];
+$GLOBALS['TL_LANG']['tl_module']['fullcal_header_center']  = ['Header (center)', $fullcalendarDesc];
+$GLOBALS['TL_LANG']['tl_module']['fullcal_header_right']   = ['Header (right)', $fullcalendarDesc];
+
+$GLOBALS['TL_LANG']['tl_module']['3 months'] = '-3 months bis +3 months';
+$GLOBALS['TL_LANG']['tl_module']['6 months'] = '-6 months bis +6 months';
+$GLOBALS['TL_LANG']['tl_module']['1 year']   = '-1 year bis +1 year';
+$GLOBALS['TL_LANG']['tl_module']['2 years']  = '-2 years bis +2 years';
+
+
