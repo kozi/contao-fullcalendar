76f7fa45760b414ddea8512adf1fc488e2d9bec4
kozi <martin@kozianka.de>
2016-10-13 11:08:01 +0200

    Added/updated some config parameter

--- contao/dca/tl_module.php
+++ contao/dca/tl_module.php
@@ -13,7 +13,7 @@
  */
 
 $fullcalPalette = '{fullcal_legend},cal_calendar,fullcal_range,cal_startDay,
-fullcal_weekMode,fullcal_aspectRatio,fullcal_weekNumbers,fullcal_isRTL;
+fullcal_weekMode,fullcal_aspectRatio,fullcal_contentHeight,fullcal_weekNumbers,fullcal_wrapTitleMonth,fullcal_isRTL;
 fullcal_header_left,fullcal_header_center,fullcal_header_right;';
 
 $GLOBALS['TL_DCA']['tl_module']['palettes']['fullcalendar']      = str_replace('{include_legend},form;', $fullcalPalette, $GLOBALS['TL_DCA']['tl_module']['palettes']['form']);
@@ -32,9 +32,11 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['fullcal_range'] = [
 $GLOBALS['TL_DCA']['tl_module']['fields']['fullcal_weekNumbers'] = [
     'label'                   => &$GLOBALS['TL_LANG']['tl_module']['fullcal_weekNumbers'],
     'exclude'                 => true,
-    'inputType'               => 'checkbox',
-    'sql'                     => "char(1) NOT NULL default ''",
+    'inputType'               => 'select',
+    'options'                 => array('none','column','within'),
+    'reference'               => &$GLOBALS['TL_LANG']['tl_module'],
     'eval'                    => ['tl_class' => 'w50'],
+    'sql'                     => "varchar(255) NOT NULL default 'next_365'",
 ];
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['fullcal_weekMode'] = [
@@ -42,8 +44,17 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['fullcal_weekMode'] = [
     'exclude'                 => true,
     'inputType'               => 'select',
     'options'                 => ['fixed', 'liquid', 'variable'],
-    'sql'                     => "varchar(255) NOT NULL default 'fixed'",
     'eval'                    => ['tl_class' => 'w50'],
+    'sql'                     => "varchar(255) NOT NULL default 'fixed'",    
+];
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['fullcal_contentHeight'] = [
+    'label'                   => &$GLOBALS['TL_LANG']['tl_module']['fullcal_contentHeight'],
+    'exclude'                 => true,
+    'default'                 => '',
+    'inputType'               => 'text',
+    'eval'                    => ['tl_class' => 'w50'],
+    'sql'                     => "varchar(255) NOT NULL default ''",    
 ];
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['fullcal_aspectRatio'] = [ // 1.35
@@ -51,16 +62,24 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['fullcal_aspectRatio'] = [ // 1.35
     'exclude'                 => true,
     'default'                 => '1.35',
     'inputType'               => 'text',
-    'sql'                     => "varchar(255) NOT NULL default ''",
     'eval'                    => ['rgxp' => 'digit', 'tl_class' => 'w50'],
+    'sql'                     => "varchar(255) NOT NULL default ''",    
 ];
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['fullcal_isRTL'] = [
     'label'                   => &$GLOBALS['TL_LANG']['tl_module']['fullcal_isRTL'],
     'exclude'                 => true,
     'inputType'               => 'checkbox',
-    'sql'                     => "char(1) NOT NULL default ''",
     'eval'                    => ['tl_class' => 'w50'],
+    'sql'                     => "char(1) NOT NULL default ''",    
+];
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['fullcal_wrapTitleMonth'] = [
+    'label'                   => &$GLOBALS['TL_LANG']['tl_module']['fullcal_wrapTitleMonth'],
+    'exclude'                 => true,
+    'inputType'               => 'checkbox',
+    'eval'                    => ['tl_class' => 'w50'],
+    'sql'                     => "char(1) NOT NULL default '1'",    
 ];
 
 
--- contao/languages/en/tl_module.php
+++ contao/languages/en/tl_module.php
@@ -1,20 +1,28 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_module']['fullcal_legend']         = 'FullCalendar';
-$GLOBALS['TL_LANG']['tl_module']['fullcal_range']          = ['Period of time', 'Period of time'];
-$GLOBALS['TL_LANG']['tl_module']['fullcal_weekNumbers']    = ['Show calendar weeks', 'Show a column with the week numbers'];
-$GLOBALS['TL_LANG']['tl_module']['fullcal_weekMode']       = ['Week mode', 'Week mode'];
-$GLOBALS['TL_LANG']['tl_module']['fullcal_aspectRatio']    = ['Aspect ratio', 'Aspect ratio [VALUE] x height'];
-$GLOBALS['TL_LANG']['tl_module']['fullcal_isRTL']          = ['Direction RTL', 'Direction RTL?'];
+$fullcalendarDesc = 'Comma seperated (attached buttons) or space (separate buttons) seperated list of following values:
+ title, today, prev, next, prevYear, nextYear,
+ month, basicWeek, basicDay, agendaWeek, agendaDay, listYear, listMonth, listWeek, listDay';
 
-$fullcalendarDesc = 'Comma seperated (attached buttons) or space (separate buttons) seperated list of following values: today, title, prev, next, month, agendaWeek, agendaDay';
-$GLOBALS['TL_LANG']['tl_module']['fullcal_header_left']    = ['Header (left)', $fullcalendarDesc];
-$GLOBALS['TL_LANG']['tl_module']['fullcal_header_center']  = ['Header (center)', $fullcalendarDesc];
-$GLOBALS['TL_LANG']['tl_module']['fullcal_header_right']   = ['Header (right)', $fullcalendarDesc];
+$GLOBALS['TL_LANG']['tl_module']['fullcal_legend']                = 'FullCalendar';
+$GLOBALS['TL_LANG']['tl_module']['fullcal_range']                 = ['Period of time', 'Period of time'];
+$GLOBALS['TL_LANG']['tl_module']['fullcal_weekNumbers']           = ['Show week numbers', 'Show week numbers'];
+$GLOBALS['TL_LANG']['tl_module']['fullcal_weekMode']              = ['Week mode', 'Week mode'];
+$GLOBALS['TL_LANG']['tl_module']['fullcal_isRTL']                 = ['Direction RTL', 'Direction RTL?'];
+$GLOBALS['TL_LANG']['tl_module']['fullcal_wrapTitleMonth']        = ['Wrap title in month view', 'Wrap title in month view?'];
+$GLOBALS['TL_LANG']['tl_module']['fullcal_contentHeight']         = ['Content height', 'Set content height. Possible values: "" (empty string, use aspect ratio, "auto" (no scrollbars), height in pixel'];
+$GLOBALS['TL_LANG']['tl_module']['fullcal_aspectRatio']           = ['Aspect ratio', 'Aspect ratio [VALUE] x height'];
 
-$GLOBALS['TL_LANG']['tl_module']['fullcal_range']['3_months'] = '-3 months bis +3 months';
-$GLOBALS['TL_LANG']['tl_module']['fullcal_range']['6_months'] = '-6 months bis +6 months';
-$GLOBALS['TL_LANG']['tl_module']['fullcal_range']['1_year']   = '-1 year bis +1 year';
-$GLOBALS['TL_LANG']['tl_module']['fullcal_range']['2_years']  = '-2 years bis +2 years';
+$GLOBALS['TL_LANG']['tl_module']['fullcal_header_left']           = ['Header (left)', $fullcalendarDesc];
+$GLOBALS['TL_LANG']['tl_module']['fullcal_header_center']         = ['Header (center)', $fullcalendarDesc];
+$GLOBALS['TL_LANG']['tl_module']['fullcal_header_right']          = ['Header (right)', $fullcalendarDesc];
 
+$GLOBALS['TL_LANG']['tl_module']['fullcal_range']['3_months']     = '-3 months bis +3 months';
+$GLOBALS['TL_LANG']['tl_module']['fullcal_range']['6_months']     = '-6 months bis +6 months';
+$GLOBALS['TL_LANG']['tl_module']['fullcal_range']['1_year']       = '-1 year bis +1 year';
+$GLOBALS['TL_LANG']['tl_module']['fullcal_range']['2_years']      = '-2 years bis +2 years';
+
+$GLOBALS['TL_LANG']['tl_module']['fullcal_weekNumbers']['none']   = 'No week numbers';
+$GLOBALS['TL_LANG']['tl_module']['fullcal_weekNumbers']['column'] = 'A column with the week numbers';
+$GLOBALS['TL_LANG']['tl_module']['fullcal_weekNumbers']['within'] = 'Week numbers within days';
 
